9d96be7064c95367ac3bf0fd566dd32c659805df
exyted1 <111304641+exyted1@users.noreply.github.com>
2024-04-16 09:24:10 +0700

    replace name folder

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from "next/head";
 
 import CardSliderOne from "../components/common/sliders/card/card-slider-one";
-import HeroSection from "../components/home-three/hero-section";
+import HeroSection from "../components/home/hero-section";
 import Statistics from "../components/tokenomics/tokenomics";
 import RoadMap from "../components/roadmap/roadmap";
 import CardSliderTwo from "../components/common/sliders/card/card-slider-two";
-import NftRoadMap from "../components/home-three/nft-roadmap";
-import Micin from "../components/home-three/micin";
+import NftRoadMap from "../components/home/nft-roadmap";
+import Micin from "../components/home/micin";
 
 
 export default function Home() {
